4d74c1d4a610a93566e2c48bde1cc3d7c0f997b5
Nam Vu <namthp99@gmail.com>
2020-01-03 11:29:06 +0700

    finish layout and route

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    render json: ["Users":{"Name":"vtnam","age":"21"}]
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  # get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
+  get  '/home',    to: 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
 end
