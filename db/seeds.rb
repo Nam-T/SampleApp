c8ac3b53c27d0f7923ad48c4830e3c4253d8b9e0
Nam Vu <namthp99@gmail.com>
2020-01-13 11:38:04 +0700

    t

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(name:  "Example User",
-    email: "example@railstutorial.org",
-    password:              "foobar",
-    password_confirmation: "foobar")
+  email: "example@railstutorial.org",
+  password:              "foobar",
+  password_confirmation: "foobar",
+  admin:     true,
+  activated: true,
+  activated_at: Time.zone.now)
 
-99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-  email: email,
-  password:              password,
-  password_confirmation: password)
+99.times do |n| 
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  ​password = "password"
+  User.create!(name: name, email:email, password:"foobar",password_confirmation:"foobar",​activated: true,activated_at: Time.zone.now)
 end
+  
