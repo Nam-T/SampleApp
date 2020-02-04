66a987271a152914632d499488d25f1168102870
Nam Vu <namthp99@gmail.com>
2020-02-04 15:29:01 +0700

    edit 3

--- test/models/authorization_test.rb
+++ test/models/authorization_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
 
 class AuthorizationTest < ActiveSupport::TestCase
-  # def setup
-  #   @user = users(:michael)
-  #   @auth = @user.authorizations.build(provider: 'facebook', uid:"My String")
-  # end
 
-  # test "should be valid" do
-  #   assert @auth.valid?
-  # end
-
-  # test "user_id, provider, uid should be present" do
-  #   @auth.user_id = nil
-  #   @auth.provider = nil
-  #   @auth.uid = nil
-  #   assert_not @auth.valid?
-  # end
 end
